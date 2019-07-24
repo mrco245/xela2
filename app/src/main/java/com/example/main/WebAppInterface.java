72ea5b88eab80ba0c9bc4a1d7a91d0d37a5a040f
Madison <mrco245@g.uky.edu>
2019-07-24 12:13:56 -0400

    add comments

--- app/src/main/java/com/example/main/WebAppInterface.java
+++ app/src/main/java/com/example/main/WebAppInterface.java
@@ -28,19 +28,9 @@ public class WebAppInterface{
     @JavascriptInterface
     public String xelaHandler(String action, String json)
     {
-
-        //build action json
-
-        //build time
-
-        //build status - pass/fail
-
-        //build data
-
-        //data_out is all of those combined
-
-
+        //Gets the action the user chose on the webpage and this switch case statement handles that
         switch(action) {
+            //Will be used to help set the color of the led light, but right now just displays the color value to the screen
             case "set-color":
                 try {
                     data_in = new JSONObject(json);
@@ -53,27 +43,34 @@ public class WebAppInterface{
 
                 break;
             case "all-data":
+                //sends all the sensor and gps data to the web server
                 rawdata = sData.sendALL();
                 break;
             case "phone-info":
+                //gathers the phone info from the users device
                 rawdata = phoneInfo.displayPhoneInfo();
                 break;
             case "time":
+                //gets the current date and time in mm/dd/yyy hh:mm:ss:nnnn
                 rawdata = time.currentDateTime();
                 break;
             case "GPS":
+                //gets the users current gps location in latitude and longitude
                 rawdata = gps.sendLocation();
                 break;
             case "SENSOR":
+                //gets the users sensor data which includes the acceleromter, magnometer, gyroscope, temperature, humidity, and pressure if supported
                 rawdata = sData.sendSensor();
                 break;
             case "temperature":
             case "weather":
+                //gets the users temperature, humidity and pressure data if supported
                 rawdata = sData.sendWeather();
                 break;
             case "acceleramoter":
             case "acc":
             case "9DOF":
+                //Just gets the accelormeter, magnometer and gyroscope data
                 rawdata = sData.sendAccelerometer();
                 break;
             default:
@@ -85,9 +82,12 @@ public class WebAppInterface{
 
         try
         {
+            //Puts the action that was called in the json object that will be sent back to the javascript handler
             data_out.put("Action", action);
+            //Puts the current time into the json object that will be sent back to the javascript handler
             data_out.put("Time", time.currentDateTime().get("Time"));
 
+            //sets the status of the function, the the raw data is filled the the status is a pass and if no data then the status is fail.
             if(rawdata.length() != 0)
             {
                  status = "Pass: data was successful";
@@ -96,8 +96,9 @@ public class WebAppInterface{
             {
                 status = "Failure: data was unsuccessful";
             }
-
+            //puts the status into the json object data_out
             data_out.put("Status", status);
+            //puts the raw data collected into the data_out json object
             data_out.put("rawdata", rawdata);
 
         }catch (Exception e)
@@ -108,6 +109,7 @@ public class WebAppInterface{
         //json that has the action, time, status, and raw data
         System.out.println(data_out.toString());
 
+        //returns the json object back to the javascript handler on the webpage
         return data_out.toString();
     }
 }
